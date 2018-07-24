d27d6a5186ff4d1cb8155723091e380237d17998
Carlos Ferreira <conta@especializati.com.br>
2018-07-24 10:13:19 -0300

    Change name class mail

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -8,7 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SendWelcomeEmail extends Mailable
+class WelcomeEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
